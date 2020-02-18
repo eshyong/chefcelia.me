dcf574e5c1102414938fc457bee328027152da80
Eric Shyong <eric.shyong@gmail.com>
2020-02-18 04:13:23 +0000

    Not sure why setcookie doesn't work 😐

--- plugins/likes.php
+++ plugins/likes.php
@@ -44,7 +44,7 @@ function add_like_button($content) {
     global $wpdb;
     $post_id = get_post()->ID;
     $like_button = "<form action='wp-json/likes/v1/posts/$post_id' method='POST'>" .
-                   "<input type='submit' value='Like this post'></input>" .
+                   "<input type='submit' value='LIKE' class='like-button'></input>" .
                    "</form>";
     $table_name = $wpdb->prefix . "likes";
     $num_likes = $wpdb->get_var(
@@ -54,13 +54,15 @@ function add_like_button($content) {
         )
     );
 
-    $like_count = "";
+    $like_count = "<p class='like-count'>";
     if ($num_likes == 1) {
-        $like_count = "<p>1 user liked this post</p>";
-    } else if ($num_likes > 1) {
-        $like_count = "<p>$num_likes users liked this post</p>";
+        $like_count .= "1 user liked this post";
+    } else if ($num_likes > 1  || $num_likes == 0) {
+        $like_count .= "$num_likes users liked this post";
     }
-    echo $like_button . $like_count . $content . $like_button;
+    $like_count .= "</p>";
+
+    echo $content . $like_button . "<br>" . $like_count;
 }
 
 function handle_like_for_post($request) {
@@ -95,12 +97,20 @@ function handle_like_for_post($request) {
             );
         }
     }
-
-    return new WP_Rest_Response(null);
 }
 
 function set_user_id_cookie() {
     $uuid = wp_generate_uuid4();
     $ten_years_in_seconds = 60 * 60 * 24 * 365 * 10;
-    setcookie('user_id', $uuid, time() + $ten_years_in_seconds);
+    setcookie(
+        'user_id',
+        $uuid,
+        array(
+            'expires' => time() + $ten_years_in_seconds,
+            'path' => '/',
+            'domain' => '.www.chefcelia.me',
+            'secure' => true,
+            'httponly' => true,
+        )
+    );
 }
